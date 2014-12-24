bf8e3d6b181309e504c720a7485f13c8d9296d29
Pciyqcn <pixelatdporygon@gmail.com>
2014-12-24 02:53:03 -0500

    first person test

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+
+	public float walkSpeed = 5.0f;
+	public float nodAngleLimit = 60.0f;
+	public float mouseSensitivity = 2.5f;
+
+	float charNod = 0;
+	// Use this for initialization
+	void Start () {
+		Screen.lockCursor = true;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/*control for aiming and moving*/
+
+		//get input from mouse for horizontal aiming
+		float charRotate = Input.GetAxis ("Mouse X") * mouseSensitivity;
+		transform.Rotate (0, charRotate, 0);
+
+		//get input from mouse for vertial rotation
+		charNod -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		charNod = Mathf.Clamp (charNod, -nodAngleLimit, nodAngleLimit);
+		Camera.main.transform.localRotation = Quaternion.Euler( charNod, 0, 0 );
+
+		//get input from keyboard for movement
+		float forwardSpeed = Input.GetAxis ("Vertical") * walkSpeed;
+		float strafeSpeed = Input.GetAxis ("Horizontal") * walkSpeed;
+
+		Vector3 movementSpeed = new Vector3 (strafeSpeed, 0, forwardSpeed );
+
+		movementSpeed = transform.rotation * movementSpeed;
+
+		CharacterController player = GetComponent <CharacterController>();
+
+		player.SimpleMove( movementSpeed );
+	}
+}
